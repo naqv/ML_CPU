cfea794159652a228a4067b195bf1cfb4f224721
Natacha Quintero <natachaquintero@Natachas-Air.home>
2019-01-20 15:57:51 +0100

    agregando cosas random

--- naty.py
+++ naty.py
@@ -9,3 +9,28 @@ except Exception as e:
 	print('error reading file')
 	print(e)
 
+#this function just load the csv 
+def load_csv():
+	try:
+		df = pd.read_csv('results.csv', delimiter = ',', low_memory = False)
+		return df
+		#metric_1 = df['vnf min cpu'] * df['min mem affinity']
+		#df = df.assign(metric_1 = metric_1.values)
+		#df.to_csv('results_out.csv', sep = ';')
+	except Exception as e:
+		print('error reading file')
+		print(e)
+
+
+def load_csv2():
+	try:
+		df = pd.read_csv('results.csv', delimiter = ',', low_memory = False)
+		metric_1 = df['vnf min cpu'] * df['min mem affinity']
+		df = df.assign(metric_1 = metric_1.values)
+		df.to_csv('results_out.csv', sep = ';')
+	except Exception as e:
+		print('error reading file')
+		print(e)
+
+df = load_csv()
+
